29dcd232c1f4049c8557633c479fc6a6c254e1c9
Ethan Gray <etan328@gmail.com>
2025-07-01 12:15:02 -0700

    Move to React

--- src/components/footerBar.svelte
+++
@@ -1,5 +0,0 @@
-<footer>
-  <div class="bg-secondary-500">
-    <h1 class="h1">This is a footer, please ignore :)</h1>
-  </div>
-</footer>
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import "../app.css";
-</script>
-
-<slot></slot>
-
-<style>
-</style>
